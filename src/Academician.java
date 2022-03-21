c3316608841e6a173fe213aebb1237c9e27574cc
Berkay <97171726+BerkaySancar@users.noreply.github.com>
2022-03-21 20:03:14 +0300

    Update Academician.java

--- src/Academician.java
+++ src/Academician.java
@@ -4,10 +4,10 @@ public class Academician extends Worker{
     private String degree;
 
     Academician(String nameSurname, String phoneNumber, String email, String department, String degree){
-        super(nameSurname, phoneNumber, email); //bir sınıfı extend ediyorsak contsturctor metodunu yazmak zorundayız
+        super(nameSurname, phoneNumber, email); //bir sınıfı extend ediyorsak constructor metodunu yazmak zorundayız.
                                                 //super metodu bu işe yarıyor.
-                                                //bunun yerine Worker sınıfında boş bir constructor daha yazabiliriz.
-                                                //--> Worker --> public Worker(){} bunu yazarsak da aynı şey oluyor.
+                                                //bunun yerine Worker sınıfında boş bir constructor da yazabiliriz.
+                                                //--> Worker --> public Worker(){} 
                                                 //super metodu constructor yazılırken en başa yazılır!
         this.department = department;
         this.degree = degree;
